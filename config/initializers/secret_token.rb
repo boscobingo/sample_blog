b3e24e19259bc1a25287e245ffe51bb3b992dbad
John <user@server>
2012-06-30 09:26:35 -0500

    Change blog to use postgres db

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-SampleBlog::Application.config.secret_token = 'acd26f7e57c72b81e72ad8a2a66e26124197e43f3ecb3e282c606ac1171113e563fa2132e3bd6b291e21f30249fab1df7208cb137f1f38e5997367ca43d03d45'
+SampleBlog::Application.config.secret_token = '5d0b911a6f036f8e886a306d556cd5b734c65ec794791d8a6bfc5863c82c63a2903f87d637aec398970a96dacfacc6f4a113fa72092ae02157f4d3677e747944'
